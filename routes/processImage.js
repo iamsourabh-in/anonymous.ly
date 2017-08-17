dee659e554469bc56076874d13ee204e8b16dbea
sourabh.rustagi <sourabh.rustagi@hotmail.com>
2017-08-18 01:58:16 +0530

    image process

--- routes/processImage.js
+++ routes/processImage.js
@@ -1,13 +1,36 @@
 var express = require('express');
 var router = express.Router();
 var fs = require('fs');
+var Jimp = require("jimp");
+
 
 /* GET users listing. */
-router.get('/', function (req, res, next) {
+router.get('/new', function (req, res, next) {
+    var imageCaption = 'Sourbabh';
+    var fileName = 'baseimage\\ccd.jpg';
+    var loadedImage;
+    // open a file called "lenna.png"
+    Jimp.read(fileName, function (err, lenna) {
+        if (err) throw err;
+        lenna.resize(256, 256)            // resize
+            .quality(100)                 // set JPEG quality
+            .greyscale()                 // set greyscale
+            .write("ccd-bw.jpg"); // save
+    });
 
-    var text2png = require('text2png');
-    fs.writeFileSync('out.png', text2png('Hello!', { textColor: 'blue' }));
-    res.send('respond with a resource');
+    Jimp.read(fileName)
+        .then(function (image) {
+            loadedImage = image;
+            return Jimp.loadFont(Jimp.FONT_SANS_16_BLACK);
+        })
+        .then(function (font) {
+            loadedImage.print(font, 20, 20, imageCaption).greyscale().posterize(5)
+                .write(fileName+'cd.jpg');
+        })
+        .catch(function (err) {
+            console.error(err);
+        });
+
 });
 
 module.exports = router;
